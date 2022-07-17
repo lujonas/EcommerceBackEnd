d512eecbb28964450d716c95f6220766a6985ad4
lujonas <47183519+lujonas@users.noreply.github.com>
2022-07-17 22:14:27 +0200

    Update ProductRepository.java

--- src/main/java/com/ludo/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/ludo/ecommerce/dao/ProductRepository.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ludo.ecommerce.entity.Product;
 
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("*")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	
 	Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
